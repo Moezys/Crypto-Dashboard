1cdbb69f79ac9d0713c77f9fd80ebb5736c0cf3f
Moezys <Moezys@users.noreply.github.com>
2025-09-27 13:15:35 +0300

    Fix ESLint configuration for CI/CD pipeline
    - Add .eslintrc.json with Next.js core web vitals configuration
    - Update eslint-config-next to latest version in package.json
    - Add Jest environment support for test files
    - Fix ESLint warning in test file with disable comment
    - All linting errors resolved - CI should now pass

--- src/components/__tests__/WatchlistPanel.test.tsx
+++ src/components/__tests__/WatchlistPanel.test.tsx
@@ -11,6 +11,7 @@ jest.mock('@/lib/watchlist', () => ({
 // Mock Next.js components
 jest.mock('next/image', () => {
   return function Image({ src, alt, ...props }: any) {
+    // eslint-disable-next-line @next/next/no-img-element
     return <img src={src} alt={alt} {...props} />;
   };
 });
